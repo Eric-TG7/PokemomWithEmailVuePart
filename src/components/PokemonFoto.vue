d226cf051f179733a7ff704156e27c7cffc765d2
Shiki <victorio1delgado@gmail.com>
2024-07-23 16:16:51 -0800

    first commit

---
+++ src/components/PokemonFoto.vue
@@ -0,0 +1,54 @@
+<template>
+<div class = "pokemon-container"></div>
+    <img :src="imgSrc" 
+        class = "hidden-pokemon"
+        alt = "pokemon">
+    <img v-if="showPokemon" 
+    :src="imgSrc"
+        class = "fade-in"
+        alt = "pokemon">
+</template>
+
+<script>
+export default {
+  props:{
+    pokemonId:{
+        type: Number,
+        required: true
+    },
+    showPokemon:{
+        type: Boolean,
+        required: true,
+        default: false
+    }
+  },
+computed:{
+    imgSrc(){
+        console.log('showPokemon:', this.showPokemon); 
+        return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${this.pokemonId}.svg`
+    }
+}
+}
+</script>
+
+<style scoped>
+img {
+    height: 200px;
+    position: absolute;
+    right: 37%;
+    user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    -webkit-user-drag: none;
+    -webkit-user-select: none;
+}
+.hidden-pokemon {
+    filter: brightness(0);
+}
+.fade-in {
+  opacity: 1;
+  transition: opacity 0.5s ease-in;
+}
+
+
+</style>
---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,35 @@
+<template>
+  <h1>Quien es este pokemon?</h1>
+  <PokemonFoto :pokemonId="45"  :showPokemon="false"/>
+  <PokemonOptions :pokemons="pokemonArr" />
+</template>
+
+<script>
+// number , required
+import PokemonOptions from '@/components/PokemonOptions.vue';
+import PokemonFoto from '@/components/PokemonFoto.vue'
+import getPokemonOptions from '@/helpers/getPokemonOptions'
+export default {
+  name: 'App',
+  components: {
+    PokemonOptions,
+    PokemonFoto
+  },
+  data(){
+return{
+  pokemonArr: []
+}
+  },
+  methods:{
+   async mixPokemonArray(){
+this.pokemonArr = await getPokemonOptions()
+    }
+  },
+  mounted(){
+  this.mixPokemonArray()
+  }
+
+}
+</script>
+
+<style></style>
