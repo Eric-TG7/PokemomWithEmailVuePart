88d5e510b03ff0df86e6fc7b9274ec1538718094
Shiki <victorio1delgado@gmail.com>
2024-08-29 12:57:37 -0800

    feat added

--- src/components/EndingEmits.vue
+++ src/components/EndingEmits.vue
@@ -19,7 +19,7 @@ export default {
    },
    methods:{
     postEmail(){
-       axios.post('http://localhost:8080/api/confirmation',{counter: this.counter, email: this.email})
+       axios.post('https://app.circleci.com/pipelines/circleci/3b5ce087-c4a4-43c3-839e-87feecf8ebc9/556393e3-a25e-4ee8-aadc-9a2bd0090dfc/2/workflows/7fc264b4-b1f6-44b4-b9b8-68317b235ff1/jobs/3',{counter: this.counter, email: this.email})
     .then(response => console.log(response.data))
     .catch(error => console.error(error));
     }
